0d31887fb0ba40d73431fcc61445da775bbbfa2f
Pallavi1234u <Pallavigavhane1234@gmail.com>
2024-07-22 15:15:35 +0530

    your message here

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter,setCounter]=useState(15)
+//let counter = 15
+const addValue=()=>{
+ //counter = counter+1
+ setCounter( prevCounter =>prevCounter+1)
+ setCounter( prevCounter =>prevCounter+1)
+ setCounter( prevCounter =>prevCounter+1)
+ setCounter( prevCounter =>prevCounter+1)
+}
+const removeValue=()=>{
+  //counter = counter+1
+  setCounter(counter-1)
+}
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+     <buttton onClick={addValue}>Add value{counter}</buttton>
+     <br/>
+     <button onClick={removeValue}>remove value {counter}</button>
+     <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App;
